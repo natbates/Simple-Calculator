0ab6e070552050d6577d13f35d5597ba98e256c6
Nathaniel Bates <nathaniel.christopher.bates@gmail.com>
2024-03-06 03:53:58 +0000

    Initial Commit

---
+++ My Calculator/My_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_CalculatorApp.swift
+//  My Calculator
+//
+//  Created by Nathaniel Bates on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct My_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
